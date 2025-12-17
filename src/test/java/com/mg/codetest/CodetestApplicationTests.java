061e3077977c1f2f9e74f09451f74208a75b0d14
Mihai Gansari <pktehgm@gmail.com>
2025-12-17 18:07:04 +0200

    some maven stuff

--- src/test/java/com/mg/codetest/CodetestApplicationTests.java
+++ src/test/java/com/mg/codetest/CodetestApplicationTests.java
@@ -1,13 +1,16 @@
 package com.mg.codetest;
 
+import com.mg.codetest.examples.a0.SimpleSpringContextLauncherApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = SimpleSpringContextLauncherApplication.class)
 class CodetestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "Application context should load successfully");
 	}
-
 }
