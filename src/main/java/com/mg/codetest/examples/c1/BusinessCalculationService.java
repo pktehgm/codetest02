88b1d8d59010d78fdebcac92b092c1ab1c4b89fb
Mihai Gansari <pktehgm@gmail.com>
2025-12-17 15:37:09 +0200

    specialized annotations

--- src/main/java/com/mg/codetest/examples/c1/BusinessCalculationService.java
+++ src/main/java/com/mg/codetest/examples/c1/BusinessCalculationService.java
@@ -1,10 +1,11 @@
 package com.mg.codetest.examples.c1;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+//@Component
+@Service
 public class BusinessCalculationService {
     DataService dataService;
 
--- src/main/java/com/mg/codetest/examples/c1/MongoDbDataService.java
+++ src/main/java/com/mg/codetest/examples/c1/MongoDbDataService.java
@@ -1,9 +1,10 @@
 package com.mg.codetest.examples.c1;
 
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository
 @Primary
 public class MongoDbDataService implements DataService{
 
